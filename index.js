d3a7eb538c1ad1891ec488e563d45ab6a6cbe55e
Benjamin <benlenormand61@gmail.com>
2024-07-27 18:19:39 +0200

    idk

--- index.js
+++ index.js
@@ -13,23 +13,80 @@ const io = new Server(server, {
     }
 });
 
+// faire un objet room avec qui contient un numéro de room, l'ensemble des users avec leur points associés, les messages ainsi que les règles de la room
+// faire un système de mise à jour des points des users
+const rooms = {};
+
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('join_room', (data) => {
-        socket.join(data.roomNumber);
-        socket.to(data.roomNumber).emit('user_connect', data);
-        console.log(`User joined room ${data.roomNumber}`);
+    socket.on('joinRoom', ({ roomName, username }) => {
+        socket.join(roomName);
+
+        if (!rooms[roomName]) {
+            rooms[roomName] = { users: [{
+                username, points: 0
+                }], messages: [], roomName: roomName};
+        } else if (!rooms[roomName].users.find(user => user.username === username)) {
+            rooms[roomName].users.push({ username, points: 0 });
+        }
+
+        io.to(roomName).emit('roomData', rooms[roomName]);
+        io.to(roomName).emit('message', { username: 'system', text: `${username} has joined the room.`, timestamp: new Date() });
+        console.log(rooms);
     });
 
-    socket.on('leave', (room) => {
-        console.log('user disconnected');
-        socket.leave(room);
-        socket.to(room).emit('user_disconnect', {pseudo: 'Utilisateur'});
+    socket.on('createRoom', ({ roomName, username, privateRoom }) => {
+        socket.join(roomName);
+        const code = generateRandomCode();
+        console.log(code);
+        rooms[roomName] = { users: [{
+                username, points: 0
+            }], messages: [], roomName: roomName, private: privateRoom, roomCode: code };
+
+        io.to(roomName).emit('roomData', rooms[roomName]);
+        console.log(rooms);
     });
-    socket.on('send_message', (data) => {
-        console.log(data);
-        socket.to(data.roomNumber).emit('receive_message', data);
+
+    socket.on('leaveRoom', ({ roomName, username }) => {
+        socket.leave(roomName);
+
+        if (rooms[roomName]) {
+            rooms[roomName].users = rooms[roomName].users.filter(user => user !== username);
+            io.to(roomName).emit('roomData', rooms[roomName]);
+            io.to(roomName).emit('message', { username: 'system', text: `${username} has left the room.`, timestamp: new Date() });
+
+            if (rooms[roomName].users.length === 0) {
+                delete rooms[roomName];
+            }
+        }
+        console.log(rooms);
     });
+    socket.on('sendMessage', ({ roomName, username, text }) => {
+        if (rooms[roomName]) {
+            const message = { username, text, timestamp: new Date() };
+            console.log(message);
+            rooms[roomName].messages.push(message);
+            io.to(roomName).emit('message', message);
+        }
+        console.log(rooms);
+    });
+});
+
+function generateRandomCode() {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 4; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+app.get('/rooms', (req, res) => {
+    const roomsArray = Object.entries(rooms).map(([roomId, roomData]) => ({
+        roomId,
+        ...roomData
+    }));
+    res.send(roomsArray);
 });
 
 server.listen(3001, () => {
